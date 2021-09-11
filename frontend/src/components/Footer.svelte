7948550c1bad6ecc5d9f0c6b5275dd709d67b3ff
jbperidy <jbperidy@hotmail.fr>
2021-09-11 12:43:47 +0200

    modify footer

--- frontend/src/components/Footer.svelte
+++ frontend/src/components/Footer.svelte
@@ -2,6 +2,20 @@
     
 </script>
 
-<div class='text-center'>
-    <p><strong>By &copy; jprdev</strong></p>
+
+<div class="row text-secondary align-items-center px-5 py-2 border-top border-secondary" style='min-height:5vh;'>
+    <div class='col-sm-12 col-md-6'><span class='fw-bold' style="font-size:1.3rem;">MENTIONS LEGALES</span></div>
+    <div class='col-sm-12 col-md-6'>
+        <div class='row my-1'>
+            <div class="col" style='font-size:1.3rem'><i class="bi bi-facebook"></i><span class='fw-bold' style="margin-left: 5px;"><a class="text-secondary" href="https://www.facebook.com/FestivalProjectionTransition/">Facebook</a></span></div>
+            <div class="col" style='font-size:1.3rem'><i class="bi bi-instagram"></i><span class='fw-bold' style="margin-left: 5px;"><a class="text-secondary" href="https://www.instagram.com/projectiontransition/">Instagram</a></span></div>
+            <div class="col" style='font-size:1.3rem'><i class="bi bi-envelope-fill"></i><span class='fw-bold' style="margin-left: 5px;"><a class="text-secondary" href="mailto:contact@projectiontransition.fr">E-mail</a></span></div>
+        </div>
+    </div>
 </div>
+
+<!-- <div class='row mt-3'>
+    <div class='col text-center'>
+        <p><strong>&copy; jprdev</strong></p>
+    </div>
+</div> -->
