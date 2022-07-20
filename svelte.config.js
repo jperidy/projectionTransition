a0e2d4720e37e0aff2b07ae68cc18c81ebe1874d
jbperidy <jbperidy@hotmail.fr>
2022-07-20 21:20:27 +0200

    remove update packages and fix port

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,7 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			envPrefix: {
-				host: 'HOST',
-				port: 'PORT'
-			}
-		}),
+		adapter: adapter(),
 	}
 };
 
